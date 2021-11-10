c84c954a756cd7786e51eb81a596bf9e38904c7e
liucath <liucath@sas.upenn.edu>
2021-11-10 00:47:32 -0500

    made instructions exercise

--- exercises/instructions/main.js
+++ exercises/instructions/main.js
@@ -3,25 +3,47 @@ let sunny = document.querySelector(".sunny");
 let cloudy = document.querySelector(".cloudy");
 let rainy = document.querySelector(".rainy");
 let snowy = document.querySelector(".snowy");
+let next = document.querySelector(".next");
+let weather = document.querySelector(".wrapper");
+let sunnyitem = document.querySelector(".sunnyitem");
+let cloudyitem = document.querySelector(".cloudyitem");
+let rainyitem = document.querySelector(".rainyitem");
+let snowyitem = document.querySelector(".snowyitem");
 
 function changeSunnyBgColor() {
 	body.classList.toggle("sunnyBg");
-	img.classList.toggle("imgChosen");
+	weather.classList.toggle("disappear");
+	cloudy.classList.toggle("disappear");
+	rainy.classList.toggle("disappear");
+	snowy.classList.toggle("disappear");
+	sunnyitem.classList.toggle("sunnyappear");
 }
 
 function changeCloudyBgColor() {
 	body.classList.toggle("cloudyBg");
-	img.classList.toggle("imgChosen");
+	weather.classList.toggle("disappear");
+	sunny.classList.toggle("disappear");
+	rainy.classList.toggle("disappear");
+	snowy.classList.toggle("disappear");
+	cloudyitem.classList.toggle("cloudyappear");
 }
 
 function changeRainyBgColor() {
 	body.classList.toggle("rainyBg");
-	img.classList.toggle("imgChosen");
+	weather.classList.toggle("disappear");
+	cloudy.classList.toggle("disappear");
+	sunny.classList.toggle("disappear");
+	snowy.classList.toggle("disappear");
+	rainyitem.classList.toggle("rainyappear");
 }
 
 function changeSnowyBgColor() {
 	body.classList.toggle("snowyBg");
-	img.classList.toggle("imgChosen");
+	weather.classList.toggle("disappear");
+	cloudy.classList.toggle("disappear");
+	rainy.classList.toggle("disappear");
+	sunny.classList.toggle("disappear");
+	snowyitem.classList.toggle("snowyappear");
 }
 
 sunny.addEventListener("click", changeSunnyBgColor);
